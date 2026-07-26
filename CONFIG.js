fcb09bd9d1dc7900ecbcb0fc293ec97ba3fc8cae
rajayokkiampandian-cmd <rajayokkiampandian@gmail.com>
2026-07-26 15:18:11 +0530

    Delete CONFIG.js

--- CONFIG.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  API_URL:    "https://script.google.com/macros/s/AKfycbyZs1GLW47mr6u5y5g903xp8huFAx0xaIdCp5l2l6U-Q1IH2_fbsmuzKJtk99xmGgTi/exec",
-  LOGIN_URL:  "",
-  SAVE_URL:   "",
-  SEARCH_URL: "",
-  REPORT_URL: ""
-};
