14aad714b4fb55857eefdd4b65d54a2687daf5c8
rajayokkiampandian-cmd <rajayokkiampandian@gmail.com>
2026-08-11 22:30:31 +0530

    Update optionb_config.js

--- app-test/optionb_config.js
+++ app-test/optionb_config.js
@@ -12,12 +12,18 @@ SCRIPT_DEPLOYMENT_ID: 'AKfycbzAbsKKNZ1hXjqb24D8rrXOATndrWhfVUSqr9jYtPbE-EVb9B_zb
   // Copy EVERY scope shown in Apps Script > Overview > Project OAuth scopes.
   // Keep openid/email so the frontend can establish the Google identity.
   OAUTH_SCOPES: [
-    'openid',
-    'email'
-    // ,'https://www.googleapis.com/auth/drive'
-    // ,'https://www.googleapis.com/auth/spreadsheets'
-    // Add the exact remaining Project OAuth scopes here.
-  ],
+  'openid',
+  'email',
+  'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive',
+  'https://www.googleapis.com/auth/script.external_request',
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/script.send_mail',
+  'https://www.googleapis.com/auth/script.scriptapp',
+  'https://www.googleapis.com/auth/script.locale',
+  'https://www.googleapis.com/auth/script.container.ui',
+  'https://www.googleapis.com/auth/documents'
+],
 
   // Production own-domain URL. Add this as an Authorized JavaScript origin
   // in the OAuth Web client.
